20611955b106376177ee15f88470bddf71aa0ab7
mdshaik851 <md939255shaik@gmail.com>
2025-06-11 02:32:11 +0000

    Add infra yaml file

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "patient_service" {
   name                 = var.repo_name_patient
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
@@ -14,6 +15,7 @@ resource "aws_ecr_repository" "patient_service" {
 resource "aws_ecr_repository" "appointment_service" {
   name                 = var.repo_name_appointment
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
