b94cfe0965b6e8a9f9486ff9304a3c96dda72680
mdshaik851 <md939255shaik@gmail.com>
2025-06-11 11:11:17 +0000

    Added output file

--- modules/cloudwatch/outputs.tf
+++ modules/cloudwatch/outputs.tf
@@ -1,9 +1,9 @@
 output "high_cpu_alarm_name" {
   description = "Name of the high CPU alarm"
-  value       = aws_cloudwatch_metric_alarm.high_cpu.alarm_name
+  value       = aws_cloudwatch_metric_alarm.cpu_utilization_high.alarm_name
 }
 
 output "high_cpu_alarm_arn" {
   description = "ARN of the high CPU alarm"
-  value       = aws_cloudwatch_metric_alarm.high_cpu.arn
+  value       = aws_cloudwatch_metric_alarm.cpu_utilization_high.arn
 }
