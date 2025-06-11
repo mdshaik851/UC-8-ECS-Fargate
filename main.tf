80cf90a995521d6a28807a5db64fc8dfa2b7d71d
mdshaik851 <md939255shaik@gmail.com>
2025-06-11 02:36:25 +0000

    Added continue with error

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
+ }
+
+  }
+  required_version = ">= 1.10.0"
+}
+
 module "vpc_creation" {
   source = "./modules/vpc"
   region = var.region
